5b4e9a9ba476502996ed29ad33a8204b924cb87c
Natalie Chan <natkuma@outlook.com>
2026-03-25 17:30:56 -0400

    completed unit5 session 1 version 1

--- Unit5/Session1/Version1.py
+++ Unit5/Session1/Version1.py
@@ -80,8 +80,13 @@ class Villager:
         self.neighbor = neighbor
 
 def message_received(start_villager, target_villager):
-    while 
-    # Nice brainstorm guys! Happy to work with you all
+    current = start_villager
+    while current is not None:
+        if current == target_villager:
+            return True
+        else:
+            current = current.neighbor
+    return False
 
 print("=============== Problem 4 =================")
 isabelle = Villager("Isabelle", "Dog", "Normal", "what's up?")
@@ -93,3 +98,130 @@ tom_nook.neighbor = kk_slider
 # isabelle --> tom_nook --> kk_slider
 print(message_received(isabelle, kk_slider))
 print(message_received(kk_slider, isabelle))
+
+
+
+
+# =============== Problem 5 =================
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+kk_slider = Node("K.K. Slider")
+harriet = Node("Harriet")
+saharah = Node("Saharah")
+isabelle = Node("Isabelle")
+
+kk_slider.next = harriet
+harriet.next = saharah
+saharah.next = isabelle
+
+print("=============== Problem 5 =================")
+print_linked_list(kk_slider)
+
+
+
+# =============== Problem 6 =================
+class Node:
+    def __init__(self, fish_name, next=None):
+        self.fish_name = fish_name
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.fish_name, end=" -> " if current.next else "\n")
+        current = current.next
+
+def catch_fish(head):
+    if head is None:
+        print("Aw! Better luck next time!")
+        return None
+    else:
+        print(f"I caught a {head.fish_name}")
+        return head.next
+
+
+print("=============== Problem 6 =================")
+fish_list = Node("Carp", Node("Dace", Node("Cherry Salmon")))
+empty_list = None
+
+print_linked_list(fish_list)
+print_linked_list(catch_fish(fish_list))
+print(catch_fish(empty_list))
+
+
+
+# =============== Problem 7 =================
+class Node:
+    def __init__(self, fish_name, next=None):
+        self.fish_name = fish_name
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.fish_name, end=" -> " if current.next else "\n")
+        current = current.next
+
+def fish_chances(head, fish_name):
+    curr = head
+    total_count = 0
+    fish_count = 0
+
+    while curr:
+        total_count += 1
+        if curr.fish_name == fish_name:
+            fish_count += 1
+        curr = curr.next
+
+    if total_count == 0:
+        return 0
+    
+    probability = fish_count / total_count
+    return round(probability, 2)
+
+
+
+print("=============== Problem 7 =================")
+fish_list = Node("Carp", Node("Dace", Node("Cherry Salmon")))
+print(fish_chances(fish_list, "Dace"))
+print(fish_chances(fish_list, "Rainbow Trout"))
+
+
+
+# =============== Problem 8 =================
+class Node:
+    def __init__(self, fish_name, next=None):
+        self.fish_name = fish_name
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.fish_name, end=" -> " if current.next else "\n")
+        current = current.next
+
+def restock(head, new_fish):
+    new_node = Node(new_fish)
+    curr = head
+    while curr.next:
+        curr = curr.next
+    curr.next = new_node
+    return head
+
+
+print("=============== Problem 8 =================")
+fish_list = Node("Carp", Node("Dace", Node("Cherry Salmon")))
+print_linked_list(restock(fish_list, "Rainbow Trout"))
